46038fe62047fbc5d37a8881d27e993c60415365
lorena2119 <189173816+lorena2119@users.noreply.github.com>
2024-12-03 07:53:50 -0500

    Tipos de datos, cómo agregar, modificar y eliminar elementos de una lista.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World") hhh
+#VARIABLES
+name = "David Gamboa" #variable String
+age = 24 #numero entero
+heigth = 1.63 #numeros decimales (float)
+trainer = True #datos booleanos
+
+#LISTAS
+hobbies = list() #Declaro variable
+hobbies = ["Cocinar","Programar","Dormir","Música"] #Vector, inicializo variable
+print(hobbies)
