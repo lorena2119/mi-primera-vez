6bf46fc3afa5a9786ede032434bc4c980fd1e7bd
lorena2119 <[189173816+lorena2119@users.noreply.github.com](mailto:189173816+lorena2119@users.noreply.github.com)>
2024-12-05 07:11:49 -0500

    Operadores lógicos

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-#VARIABLES
-name = "Lorena Contreras" #variable String
-age = 16 #numero entero
-heigth = 1.65 #numeros decimales (float)
-trainer = False #datos booleanos
+#Operadores Lógicos and or not
+edad=int(input("Ingrese su edad: "))
 
-#LISTAS
-hobbies = list() #Declaro variable
-hobbies = ["Cocinar","Programar","Dormir","Música"] #Vector, inicializo variable
-print(hobbies)
+#if(edad>= 18 and edad<=20 or edad>=24 and edad<=30 or edad>=34 and edad<=45):
+#   print("Credito como pasante")
+
+if(edad == 17):
+    print("Puede solicitar un crédito como pasante")
+
+elif(edad<20 and edad>=18):
+    print("Puede solicitar un crédito")  
+
+elif(edad<=24 and edad>=20):
+     print("Puede solicitar un crédito y bono")      
+
+
+else:
+    print("No puede solicitar un crédito.")
